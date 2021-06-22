6efe110b51b2d7a9a8b578ebe90e30aa2fc98dd9
murky0610 <61217400+murky0610@users.noreply.github.com>
2021-06-23 05:26:44 +0800

    Delete mmlp/mmlp/booking directory

--- mmlp/mmlp/booking/home.php
+++
@@ -1,6 +0,0 @@
-        <!-- <header class="masthead"> -->
-            <div style="margin: 0 auto;">
-                <?php include 'movie_carousel.php' ?>
-            </div>
-        <!-- </header>
- -->
