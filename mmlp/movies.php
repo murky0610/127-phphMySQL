0428eb13fb223a33fd8f47115dc3a8e4387eee15
murky0610 <61217400+murky0610@users.noreply.github.com>
2021-06-29 20:13:53 +0800

    Add files via upload

---
+++ mmlp/movies.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MMLP</title>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,700' rel='stylesheet' type='text/css'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Girassol&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles_movies.css">
+</head>
+    
+<body>
+	<header id="top_header" class="clearfix">
+		<div class="wrapper">
+			<h1 class="logo"> <img src="images/MMLP.png" class="logo" style="width:25px;height:25px;margin-right: 5px"> <a href="index.php">MMLP</a>   </h1>
+			<nav id="main_nav">
+                <a href="booking/booking.php">Book Now!</a>
+                <a href="user_settings.php">User</a>
+				<a href="movies.php">Movies</a>
+				<a href="faq.php">FAQ</a>
+			</nav>
+		</div>
+	</header>
+
+	<section id="top_movies" class="clearfix">
+		<div class="wrapper">
+            <h2 style="text-align: center; font-weight: 510">Now Showing:</h2>
+			<div class="row">
+			
+			<div class="post">
+				<form>
+			<button type="submit" formaction="overview/malcolm.php"class="scanfcode">
+				<img src="images/movies/malcolm.jpg" alt="The Martian">
+				
+				<h3 class="title">Malcolm & Marie</h3>
+				<p class="post_info">R | Drama , Romance <br> June 25<br> 1 hour, 54 minutes<br> <span><b>10:30 am - 1:00 pm</b></span> </p>
+</button>
+			</div>
+
+</div>
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/raya.php"class="scanfcode">
+				<img src="images/movies/raya.jpg" alt="Inside Out">
+				<h3 class="title">Raya and The Last Dragon</h3>
+				<p class="post_info">PG | Anime , Adventure <br> June 19<br> 2 hours <br> <span><b>1:30 pm - 3:00 pm</b></span></p>
+			
+			</div>
+			</button>
+		</div>
+
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/mitchell.php"class="scanfcode">
+				<img src="images/movies/mitchell.jpg" alt="Werewolves Within">
+				<h3 class="title">The Mitchells vs. the Machines</h3>
+				<p class="post_info">U | Anime , Comedy <br> June 19<br> 2 hours<br><span><b>3:30 pm - 5:00 pm</b></span></p>
+</button>
+			</div>
+		</div>
+		<div class="row">
+	
+		
+			<div class="post">
+			<button type="submit" formaction="overview/outside.php"class="scanfcode">
+				<img src="images/movies/out_wire.jpg" alt="Mad Max">
+				<h3 class="title" style="">Outside the Wire</h3>
+				<p class="post_info">R | Action , Fantasy <br> June 19 <br>  1 hour, 54 minutes<br><span><b>5 pm - 6:30 pm</b></span></p>
+</button>
+			</div>
+		
+
+	
+		</div>
+		
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/little.php"class="scanfcode">
+				<img src="images/movies/little.jpg" alt="Star Wars">
+				<h3 class="title">The Little Things</h3>
+				<p class="post_info">15 | Crime , Drama <br> June 19 <br>  2 hours, 12 minutes<br><span><b>7:00 pm - 8:30 pm</b></span></p>
+
+			</div>
+			</button>
+		</div>
+
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/mk11.php"class="scanfcode">
+				<img src="images/movies/MK11.jpg" alt="The Avengers">
+				<h3 class="title">Mortal Kombat 11</h3>
+				<p class="post_info">15 | Action , Fantasy <br> June 25 <br>  1 hour, 49 minutes<br><span><b>8:30 pm - 11:30 pm</b></span></p>
+		</button>
+			</div>
+		</div>
+		
+
+		</div>
+	</section>
+    
+    <hr>
+    
+	<section id="top_movies" class="clearfix">
+		<div class="wrapper">
+		<h2 style="text-align: center; font-weight: 510">Showing Soon:</h2>
+            
+		<div class="row">
+			
+			<div class="post">
+				<form>
+			<button type="submit" formaction="overview/f9.php"class="scanfcode">
+				<img src="images/movies/f9.jpg" alt="The Martian">
+				
+				<h3 class="title">Fast and Furious 9</h3>
+				<p class="post_info">PG13 | Action , Genre <br> 2 hours, 25 minutes <br>  </p>
+</button>
+			</div>
+
+</div>
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/false_positive.php"class="scanfcode">
+				<img src="images/movies/fp.jpg" alt="Inside Out">
+				<h3 class="title">False Positive</h3>
+				<p class="post_info">R | Horror , Thriller <br> 1 hour, 32 minutes <br> </p>
+			
+			</div>
+			</button>
+		</div>
+
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/werewolves_within.php"class="scanfcode">
+				<img src="images/movies/ww.jpg" alt="Werewolves Within">
+				<h3 class="title">Werewolves Within</h3>
+				<p class="post_info">R | Comedy , Horror<br> 1 hour, 40 minutes<br></p>
+</button>
+			</div>
+		</div>
+		<div class="row">
+	
+		
+			<div class="post">
+			<button type="submit" formaction="overview/felix_treasure.php"class="scanfcode">
+				<img src="images/movies/fathd.jpg" alt="Mad Max">
+				<h3 class="title" style="">Felix And The Hidden Treasure</h3>
+				<p class="post_info">PG | Family , Animation <br>  1 hour, 30 minutes<br></p>
+</button>
+			</div>
+		
+
+	
+		</div>
+		
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/against_the_current.php"class="scanfcode">
+				<img src="images/movies/atc.jpg" alt="Star Wars">
+				<h3 class="title">Against The Current</h3>
+				<p class="post_info">NR | Documentary <br>  1 hour, 30 minutes<br></p>
+
+			</div>
+			</button>
+		</div>
+
+		<div class="row">
+			<div class="post">
+			<button type="submit" formaction="overview/i_carry.php"class="scanfcode">
+				<img src="images/movies/icywm.jpg" alt="The Avengers">
+				<h3 class="title">I Carry You With Me</h3>
+				<p class="post_info">R | Drama  <br>  1 hour, 50 minutes<br></p>
+		</button>
+			</div>
+		</div>
+            
+		</div>
+        
+	</section>
+
+	<footer id="main_footer">
+		<p class="logo">MMLP</p>
+		<p class="copyright">&copy;2021 MMLP. All Rights Reserved.</p>
+		<div class="links">
+            <a href="#">About Us</a>
+			<a href="#">Terms of Service</a>
+			<a href="#">Privacy Policy</a>
+		</div>
+	</footer>
+	 <!-- <div class="row">
+				<div class="post">
+				<button type="submit" formaction="overview/werewolves_within.php"class="scanfcode1">
+					<img src="images/movies/boss_baby.jpg" alt="The Avengers">
+					<h3 class="title">The Boss Baby: Family Business</h3>
+					<p class="post_info">PG | Comedy , Family , Animation <br> July 2<br>
+					</button>			</div>
+			</div>
+
+			<div class="row">
+				<div class="post">
+				<button type="submit" formaction="overview/werewolves_within.php"class="scanfcode1">
+					<img src="images/movies/forever_purge.jpg" alt="Inside Out">
+					<h3 class="title">The Forever Purge</h3>
+					<p class="post_info">R | Horror , Thriller <br> July 2<br> 
+					</button>		</div>
+			</div>
+            
+            <div class="row">
+				<div class="post">
+				<button type="submit" formaction="overview/werewolves_within.php"class="scanfcode1">
+					<img src="images/movies/summer_soul.jpg" alt="The Martian">
+					<h3 class="title">Summer of Soul</h3>
+					<p class="post_info">PG-13 | Documentary , Music <br> July 2<br>
+					</button>	</div>
+			</div>
+            
+            <div class="row">
+				<div class="post">
+				<button type="submit" formaction="overview/werewolves_within.php"class="scanfcode1">
+					<img src="images/movies/black_widow.jpg" alt="Mad Max">
+					<h3 class="title">Black Widow</h3>
+					<p class="post_info">PG-13 | Action , Adventure , Superhero <br> July 9<br> 
+					</button>		</div>
+			</div>
+
+            <div class="row">
+				<div class="post">
+				<button type="submit" formaction="overview/werewolves_within.php"class="scanfcode1">
+					<img src="images/movies/audible.jpg" alt="Star Wars">
+					<h3 class="title">Audidble</h3>
+					<p class="post_info">PG-13 | Documentary <br> July 1<br>
+					</button>	
+				</div>
+			</div>
+            
+			<div class="row">
+				<div class="post">
+				<button type="submit" formaction="overview/werewolves_within.php"class="scanfcode1">
+					<img src="images/movies/zola.jpg" alt="Jurassic World">
+					<h3 class="title">Zola</h3>
+					<p class="post_info">R | Drama , Comedy , True Story <br> June 30<br>
+</button>	
+				</div>
+			</div>	 -->
+</body>
+</html>
